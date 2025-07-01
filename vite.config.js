673a0d1eeeafc958addd9abb9c20606b009b2db7
Dhvani Thakkar <dhvanithakkar2003@gmail.com>
2025-07-01 14:26:43 -0500

    refactor

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dhvani-thakkar-portfolio/', // 👈 use your repo name here
   plugins: [react()],
-   build: {
-    outDir: 'docs',
-    emptyOutDir: true
+  base: '/dhvani-thakkar-portfolio/', // Add this line
+  build: {
+    outDir: 'docs', // Change from 'dist' to 'docs'
+    emptyOutDir: true,
   }
-});
+})
