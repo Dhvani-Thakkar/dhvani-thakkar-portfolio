5fa90c095e471a631fb28b3fd432d9bd23cb7682
Dhvani Thakkar <dhvanithakkar2003@gmail.com>
2025-07-01 13:51:42 -0500

    refactor: changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/dhvani-thakkar-portfolio/', // 👈 use your repo name here
   plugins: [react()],
+  build: {
+  outDir: 'docs',
+}
 });
